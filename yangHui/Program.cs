4da0fe928769640f4819c934ab246f4b24223363
li-sifeng <lisifeng@126.com>
2021-12-19 11:12:06 +0800

    Add yangHui which used to generate Yanghui triaggle

---
+++ yangHui/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Yanghui
+{
+    public class yanghui
+    {
+        public static void Main(string[] arg)
+        {
+            int n = 0;
+            while (0 == n){
+                try {
+                    Console.WriteLine("Please input line num of YangHui-Triagle:");
+                    n = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException) {
+                    Console.WriteLine("Invalid number, try again!");
+                }
+            }
+            Console.WriteLine("Total {0} line!", n);
+            int[] nums = new int[n];
+            nums[0] = 1;
+            for (int i=1; i<=n; i++)
+            {
+                for (int j=n-i; j>0; j--)
+                {
+                    Console.Write("  ");
+                }
+                for (int j=i-1; j>0; j--)
+                {
+                    nums[j] += nums[j-1];
+                    Console.Write("{0}  ", nums[j]);
+                }
+                Console.WriteLine("1");
+            }
+        }
+    }
+}
