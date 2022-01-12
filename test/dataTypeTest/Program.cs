a4e11976194e0245729264590c15a8c4c8145364
li-sifeng <lisifeng@126.com>
2022-01-12 19:46:49 +0800

    Add data type testing code

---
+++ test/dataTypeTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DataTypeTest
+{
+    public class MyDataTypeTest
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"[int] Type valid range is {int.MinValue} ~ {int.MaxValue}");
+            Console.WriteLine($"[long] Type valid range is {long.MinValue} ~ {long.MaxValue}");
+            Console.WriteLine($"[float] Type valid range is {float.MinValue} ~ {float.MaxValue}");
+            Console.WriteLine($"[double] Type valid range is {double.MinValue} ~ {double.MaxValue}");
+            Console.WriteLine($"[decimal] Type valid range is {decimal.MinValue} ~ {decimal.MaxValue}");
+        }
+    }
+}
