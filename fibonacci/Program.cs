b051e143302a4135fd8415b527c514455f93fc5c
li-sifeng <lisifeng@126.com>
2022-01-12 20:47:47 +0800

    Add Fibonacci test code

---
+++ fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FibonacciTest
+{
+    public class MyFibonacciTest
+    {
+        public static int FibonacciFun(int n)
+        {
+            if (0 == n)
+            {
+                return 0;
+            }
+            if (1 == n)
+            {
+                return 1;
+            }
+            else
+            {
+                return FibonacciFun(n-1) + FibonacciFun(n-2);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string? input = Console.ReadLine();
+            if (input != null)
+            {
+                int num = Convert.ToInt32(input);
+                Console.WriteLine($"Fibonacci({num}) = {FibonacciFun(num)}");
+            }
+        }
+    }
+}
