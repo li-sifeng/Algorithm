f0313378545e5ffef378a3eca5a8311b93297903
li-sifeng <lisifeng@126.com>
2022-01-12 19:35:24 +0800

    add test folder for testing purpose only

---
+++ test/reflectionTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace MyTest 
+{
+    public class MyTestClass
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+                int methodCount = 0;
+                foreach(var t in a.DefinedTypes)
+                {
+                    methodCount += t.GetMethods().Count();
+                }
+                Console.WriteLine($"{a.DefinedTypes.Count()} types with {methodCount} methods in {r.Name} assembly");
+            }
+        }
+    }
+}
