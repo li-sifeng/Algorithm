539b46a1433e42e114f3941194172cfa334ea869
li-sifeng <lisifeng@126.com>
2022-01-12 20:03:42 +0800

    add OS detection sample code

---
+++ test/OSDetection/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyOSDetection
+{
+    public class OSDetectionTest
+    {
+        public static void Main(string[] args)
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                Console.WriteLine("Your OS is Windows");
+            }
+            else if (OperatingSystem.IsAndroid())
+            {
+                Console.WriteLine("Your OS is Android");
+            }
+            else if (OperatingSystem.IsLinux())
+            {
+                Console.WriteLine("Your OS is Linux");
+            }
+            else if (OperatingSystem.IsIOS())
+            {
+                Console.WriteLine("Your OS is iOS");
+            }
+        }
+    }
+}
